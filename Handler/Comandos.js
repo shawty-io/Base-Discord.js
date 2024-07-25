5a9cbdd7ed2baa77d4e58905ed98d4b38494d233
Floexp <148739269+floexp@users.noreply.github.com>
2024-07-25 16:24:48 -0300

    Update Comandos.js

--- Handler/Comandos.js
+++ Handler/Comandos.js
@@ -6,18 +6,19 @@ module.exports = async (client) => {
     fs.readdir(`./Comandos`, (error, folder) => {
         folder.forEach((subfolder) => {
             fs.readdir(`./Comandos/${subfolder}/`, (error, files) => {
-                files.forEach((files) => {
-                    if (!files?.endsWith(".js")) return
-                    files = require(`../Comandos/${subfolder}/${files}`)
-                    if (!files?.name) return
-                    client.slashCommands.set(files?.name, files)
+                files.forEach((file) => {
+                    if (!file?.endsWith(".js")) return
+                    const command = require(`../Comandos/${subfolder}/${file}`)
+                    if (!command?.name) return
+                    client.slashCommands.set(command.name, command)
 
-                    SlashsArray.push(files)
+                    SlashsArray.push(command)
                 })
             })
         })
     })
+
     client.on("ready", async () => {
-        client.guilds.cache.forEach((guild) => guild.commands.set(SlashsArray))
+        await client.application.commands.set(SlashsArray)
     })
 }
