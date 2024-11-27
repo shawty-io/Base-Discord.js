e3ad22a3143714d69fd9107eb5cf0c1764b4c7d1
Shawty <148739269+shawty-io@users.noreply.github.com>
2024-11-27 09:07:55 -0300

    Update

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ require('dotenv').config()
 const Discord = require('discord.js')
 
 const client = new Discord.Client({
-    intents: []
+    intents: [
+        Discord.GatewayIntentBits.Guilds,
+        Discord.GatewayIntentBits.GuildMembers
+    ]
 })
 
 module.exports = client
 
 client.slashCommands = new Discord.Collection()
-require('./Handler/Comandos')(client)
-require('./Handler/Eventos')(client)
+require('./src/Handler/Comandos')(client)
+require('./src/Handler/Eventos')(client)
 
 client.login(process.env.TOKEN)
